433a66b5d2dbf8d577864206bc32f47934860c0c
HaroldObasi <haroldobasi2k16@gmail.com>
2023-10-01 01:33:18 +0100

    Inital commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <p className='p-5 bg-red-500'>hello word</p>
+    </div>
+  )
+}
+
+export default App
