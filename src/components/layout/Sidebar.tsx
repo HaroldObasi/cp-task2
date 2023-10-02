e078955d5a5e382f4bd10317b10174acca3c7150
HaroldObasi <haroldobasi2k16@gmail.com>
2023-10-02 01:16:55 +0100

    re arranged icons

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,29 +1,29 @@
 import calendarCheck from "../../assets/sidebar/Calendar_Check.svg";
 import chevronRight from "../../assets/sidebar/Chevron_Right_MD.svg";
 import fileDocument from "../../assets/sidebar/File_Document.svg";
+import usersGroup from "../../assets/sidebar/Users_Group.svg";
 import heart from "../../assets/sidebar/Heart_01.svg";
 import house from "../../assets/sidebar/House_01.svg";
 import notebook from "../../assets/sidebar/Notebook.svg";
 import settings from "../../assets/sidebar/Settings.svg";
 import shareAndroid from "../../assets/sidebar/Chevron_Right_MD.svg";
-import usersGroup from "../../assets/sidebar/Share_Android.svg";
+import share from "../../assets/sidebar/Share_Android.svg";
 
 type Props = {};
 
 const Sidebar = (props: Props) => {
   const options = [
     { icon: house },
-    { icon: usersGroup },
+    {
+      icon: usersGroup,
+    },
     {
       icon: calendarCheck,
     },
-    {
-      icon: chevronRight,
-    },
+    { icon: share },
     { icon: fileDocument },
-    { icon: heart },
     { icon: notebook },
-    { icon: settings },
+    { icon: heart },
     { icon: shareAndroid },
   ];
 
@@ -39,7 +39,7 @@ const Sidebar = (props: Props) => {
             <button
               className={`${
                 index === 0 && "bg-[#E9EFFF]"
-              } hover:bg-[#E9EFFF] p-[12px] rounded-lg mb-[24px]`}
+              } hover:bg-[#E9EFFF] p-[12px] rounded-lg mb-[10px]`}
               key={index}
             >
               <img src={item.icon} alt="Sidemenu option" />
